aedc0d3a75ab96d5178d26968850c615c1b785d6
maghayei83 <127246933+maghayei83@users.noreply.github.com>
2023-05-25 05:56:16 -0700

    Add files via upload

---
+++ ccod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication51
+{
+    class employer
+    {
+        int employeeid{set; get;}
+
+        date date{set; get;}
+
+        string reason{set; get}
+    }
+}
+class randomGenerator
+{
+
+    static void main()
+    {
+        System.Random random = new System.Random();
+        System.Console.WriteLine(random.Next());
+        System.Console.WriteLine(random.Next(50));
+        System.Console.WriteLine(random.Next(10, 50));
+    }
+}
